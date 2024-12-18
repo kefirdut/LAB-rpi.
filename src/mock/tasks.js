43aeb1efb4d2b09a605650e7c011bb893331b726
kefirdut <166387956+kefirdut@users.noreply.github.com>
2024-12-18 14:25:32 +0300

    Add files via upload

---
+++ src/mock/tasks.js
@@ -0,0 +1,52 @@
+export const tasks = [
+  {
+    id: "1",
+    title: "11111",
+    status: "backlog",
+  },
+  {
+    id: "2",
+    title: "22222",
+    status: "inProgress",
+  },
+  {
+    id: "3",
+    title: "333333",
+    status: "inProgress",
+  },
+  {
+    id: "4",
+    title: "444444",
+    status: "backlog",
+  },
+  {
+    id: "5",
+    title: "55555",
+    status: "trash",
+  },
+  {
+    id: "6",
+    title: "66666666",
+    status: "backlog",
+  },
+  {
+    id: "7",
+    title: "77777",
+    status: "trash",
+  },
+  {
+    id: "8",
+    title: "888888",
+    status: "inProgress",
+  },
+  {
+    id: "9",
+    title: "999999",
+    status: "inProgress",
+  },
+  {
+    id: "10",
+    title: "10101010",
+    status: "trash",
+  },
+];
